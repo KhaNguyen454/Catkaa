2736eb7ff9df8ae2877359c57031f2ae0c711b44
khanguyen <143949961+KhaNguyen454@users.noreply.github.com>
2026-08-20 01:39:39 +0700

    fix: Add AllowCredentials to CORS policy

--- BE/Catkaa.MicroPms.Api/Program.cs
+++ BE/Catkaa.MicroPms.Api/Program.cs
@@ -56,7 +56,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins("http://localhost:5173", "https://catkaa.vercel.app")
               .AllowAnyMethod()
-              .AllowAnyHeader();
+              .AllowAnyHeader()
+              .AllowCredentials();
     });
 });
 
